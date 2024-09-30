a229d010bd6ba8a2cd5959b45a58df2a26b85e32
Prarthan P <68778846+PrarthanP@users.noreply.github.com>
2024-09-30 22:17:15 +0530

    removed trailing /

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin : ['http://localhost:3000', 'https://blog-website-sage-iota.vercel.app/'],
+    origin : ['http://localhost:3000', 'https://blog-website-sage-iota.vercel.app'],
     methods : ['GET, POST, PUT, DELETE'],
     credentials: true,
 }));
